fea1220e5aefbf758d71a04fb0a72623fcd89bbb
ROJAS CENEDESE MARTINA <47695905@est.ort.edu.ar>
2023-10-23 16:09:52 -0300

    tengo poca bateria

--- Front-End/startapp/app/page.tsx
+++ Front-End/startapp/app/page.tsx
@@ -20,42 +20,48 @@ export default function Home () {
           <li className="ml-2 opacity-75 hover:opacity-100">Projects</li>
           <li className="ml-2 opacity-75 hover:opacity-100">People</li>
           <li className="ml-2 opacity-75 hover:opacity-100">About</li>
+          <svg xmlns="http://www.w3.org/2000/svg" width="47" height="47" viewBox="0 0 50 48" fill="none">
+          <circle cx="18.145" cy="17.75" r="16.5" stroke="white" stroke-width="2"/> 
+          <path d="M30.645 29.25L48.645 46.75" stroke="white" stroke-width="2"/>
+        </svg>
         </ul>
 
         <div className="flex flex-row list-none gap-[40px] text-[20px] items-center">
-        {
-          <button className=" text-primaryv static py-[10px] px-[30px] text-[20px] rounded-[17px] justify-center items-center bg-transparent border-2 border-primaryv font-red">
+        <Image className= 'w-[3px] h-[46px]'
+            src="/img/palito.png"
+            alt=""
+            width = {200}
+            height= {100}
+          />
+
+          <button className=" text-primaryv py-[10px] px-[30px] text-[20px] rounded-[17px] justify-center items-center border-[1px] border-primaryv hover:border-[2px] font-red">
            <Link href="/signup">Sign In</Link>
           </button>
-        }
-        </div> 
-        <div className="flex flex-row list-none gap-[40px] text-[20px] items-center">
-        {
-          <button className="static py-[10px] px-[30px] text-[20px] rounded-[17px] justify-center items-center bg-transparent border-slate-500 hover:bg[#151515] font-red">
+
+          <button className=" hover:py-[10px] px-[30px] text-[20px] rounded-[17px] justify-center items-center font-red">
            <Link href="/login">Log In</Link>
           </button>
-        }
+        
         </div>  
       </nav>
     </header>
     <section className="pl-24 bg-gradient-to-br from-black from-50% to-primaryv w-full h-[full]">
-      <h1 className="mt-[184px] mb-[27px] flex flex-col static w-[838px]text-white text-[70px] justify-center font-semibold text-7xl font-red">Build your own team project</h1>
+      <h1 className="mt-[184px] mb-[27px] flex flex-col w-[838px]text-white text-[70px] justify-center font-semibold text-7xl font-red">Build your own team project</h1>
       <div>
-      <p className="static mb-[47px] font-red w-[829px] h-[80px] font-red text-white text-[30px]">
+      <p className="mb-[47px] font-red w-[829px] h-[80px] font-red text-white text-[30px]">
         Connect ideas with people to create starups and unique entrepeneurships.
       </p>
       </div>
       <div className="flex itemx-center gap-2">
-        {
-          <button className="static w-[185px] h-[67px] rounded-[20px] flex justify-center items-center bg-primaryv hover:opacity-75 font-semibold">
-           <Link className=" static text-black font-red font-normal text-[25px] text-center my-4 mx-6" href="/signup">Get Started</Link>
+        
+          <button className="w-[185px] h-[67px] rounded-[20px] flex justify-center items-center bg-primaryv">
+           <Link className=" text-black font-red font-normal text-[25px] text-center my-4 mx-6" href="/signup">Get Started</Link>
           </button>
-        }
-        {
-          <button className=" static w-[185px] h-[67px] rounded-[17px] flex justify-center items-center bg-trasparent decoration-solid hover:opacity-75 font-semibold">
-           <Link className=" static text-black font-red font-normal text-[25px] text-center my-4 mx-6" href="/signup">Learn About</Link>
+    
+          <button className="w-[185px] h-[67px] rounded-[17px] flex justify-center items-center underline font-semibold">
+           <Link className="text-primaryv font-red font-normal text-[25px] text-center my-4 mx-6" href="/signup">Learn About</Link>
           </button>
-        }
+        
       </div>
       <Image className= 'w-[881px] h-[776px] ml-[350px]'
             src="/img/Imagen Vectorial.png"
